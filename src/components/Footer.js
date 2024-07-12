f0cca7261ac8873e437ed8f384a885ca4a5b23be
Aadesh Rajbhandari <cresthaadork124@gmail.com>
2024-07-13 01:35:16 +1000

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+            <p>
+                Copyright &copy; 2024  
+            </p>
+            <Link to="/about"></Link>
+    </footer>
+  )
+}
+
+export default Footer
